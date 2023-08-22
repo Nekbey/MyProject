effabac94757426cd58feb3e1464601afbab99e1
Nekbey <141661225+Nekbey@users.noreply.github.com>
2023-08-22 21:43:14 +0300

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,3 @@
+cd C:\Users\1\IdeaProjects\Project_2\out\artifacts\Project_2_jar
+java --module-path "C:\Users\1\Downloads\openjfx-17.0.8_windows-x64_bin-sdk\javafx-sdk-17.0.8\lib" --add-modules javafx.controls,javafx.fxml -jar Project_2.jar
+pause
